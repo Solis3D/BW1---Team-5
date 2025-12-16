93aae197f499d88315b404a0454fde54b8c6158d
Samuel-Valentini <samuelvitalia@gmail.com>
2025-12-16 16:30:26 +0100

    v 0.12 first timer version without graphics

--- assets/scripts/timer.js
+++ assets/scripts/timer.js
@@ -1,1 +1,23 @@
 console.log("timer online");
+
+function timer(seconds = 30, bool = true) {
+  // secondo è il numero di secondi da cronometrare,
+  // bool è una variabile che dopo la promise diventerà falsa
+  return new Promise((end) => {
+    const time = setInterval(() => {
+      seconds = seconds - 25 / 1000;
+      //   qui va inserito codice da eseguire durante (ogni 25 millisecondi)
+      //
+      //
+      //
+      if (seconds <= 0) {
+        clearInterval(time);
+        bool = false;
+        // qui va inserito eventuale codice da eseguire alla fine
+        end(bool);
+      }
+    }, 25);
+  });
+}
+
+timer(5, true).then((result) => console.log(result));
